c4a121a7487b51b7a2137a8e1e7c69a92c1e4a3c
aiaxcartpremium <shanaiamau99@gmail.com>
2025-10-31 12:51:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// EDIT these:
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 export const SUPABASE_URL = "https://dgiakdurpdjtlpwudhif.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnaWFrZHVycGRqdGxwd3VkaGlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4NDEwNDcsImV4cCI6MjA3NzQxNzA0N30.L9hRV4sJc4IP8dnZ8EGCCzcWCv7v7zUlfQzXIASKhA4";
-
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 // Social links shown in sidebar
 export const SOCIALS = {
   tg:  "https://t.me/shanmau",
